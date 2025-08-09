e8a2af9a5f173c061d521f4da33792f95ae9a69e
Mohzcrea8me <mohzcrea8me@gmail.com>
2025-08-09 18:28:33 +0100

    feat: Web deep link init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const params = new URLSearchParams(window.location.search);
 const uri = params.get("uri");
 
+console.log("vw: ", window.verifiedwallet);
+
 if (uri?.startsWith("wc") && window.verifiedwallet?.request) {
   console.log("using uri: ", uri);
   window.verifiedwallet.request({
